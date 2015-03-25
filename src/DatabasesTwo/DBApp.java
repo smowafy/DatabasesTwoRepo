98683781ab0d26922e4f92b08ac8b00759988cb4
Sherif Mowafy <sherif.mowafy@gmail.com>
2015-03-25 16:18:37 +0200

    first commit
    uploading initial project layout

---
+++ src/DatabasesTwo/DBApp.java
@@ -0,0 +1,5 @@
+package DatabasesTwo;
+
+public class DBApp {
+	
+}
---
+++ src/DatabasesTwo/Page.java
@@ -0,0 +1,12 @@
+package DatabasesTwo;
+
+import java.io.Serializable;
+
+public class Page implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
