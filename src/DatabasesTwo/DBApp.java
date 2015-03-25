384f7846a2cf19fb67eadf072cdea572127eeb44
Sherif Mowafy <sherif.mowafy@gmail.com>
2015-03-25 21:02:48 +0200

    Adds methods to DPApp java'

--- src/DatabasesTwo/DBApp.java
+++ src/DatabasesTwo/DBApp.java
@@ -1,5 +1,28 @@
 package DatabasesTwo;
 
 public class DBApp {
-	
+	public void init() {
+
+	}
+	public void createTable(String strTableName, Hashtable<String,String> htblColNameType, Hashtable<String,String>htblColNameRefs, String strKeyColName) throws DBAppException {
+
+	}
+	public void createIndex(String strTableName, String strColName) throws DBAppException {
+
+	}
+	public void insertIntoTable(String strTableName, Hashtable<String,String> htblColNameValue) throws DBAppException {
+
+	}
+	public void deleteFromTable(String strTableName, Hashtable<String,String> htblColNameValue, String strOperator) throws DBEngineException {
+
+	}
+	public Iterator selectValueFromTable(String strTable, Hashtable<String,String> htblColNameValue, String strOperator) throws DBEngineException {
+
+	}
+	public Iterator selectRangeFromTable(String strTable, Hashtable<String,String> htblColNameRange, String strOperator) throws DBEngineException {
+
+	}
+	public void saveAll() throws DBEngineException {
+		
+	}
 }
